95e28f1a15e5a46b9ac1f373c46ece2bee62b416
gilmanalfathir <gilmanalfathir23@gmail.com>
2024-12-07 16:10:36 +0700

    services n api

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -2,4 +2,6 @@ import 'package:flutter/foundation.dart' show immutable;
 
 @immutable
 class Constants {
+  static const String apiKey = '41a2c037cabe865f5af490bf6f2919d0';
+  static const String apiKey2 = '0be77bfc5b44582e9ef595c67a9c9348';
 }
