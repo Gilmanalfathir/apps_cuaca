23fed369e56ee0d5645b60270168e5871e43956c
gilmanalfathir <gilmanalfathir23@gmail.com>
2024-12-12 01:38:27 +0700

    add back button

--- lib/screens/weather_detail_screen.dart
+++ lib/screens/weather_detail_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter/services.dart';
 
 import '/constants/text_styles.dart';
 import '/extensions/datetime.dart';
@@ -20,74 +21,93 @@ class WeatherDetailScreen extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final weatherData = ref.watch((cityForecastProvider(cityName)));
 
+    // Make the status bar transparent
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light.copyWith(
+      statusBarColor: Colors.transparent,
+    ));
+
     return Scaffold(
-      body: weatherData.when(
-        data: (weather) {
-          return GradientContainer(
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
+      body: Stack(
+        children: [
+          weatherData.when(
+            data: (weather) {
+              return GradientContainer(
                 children: [
-                  const SizedBox(
-                    height: 30,
-                    width: double.infinity,
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      const SizedBox(
+                        height: 80, // Adjust for status bar space
+                        width: double.infinity,
+                      ),
+                      // Country name text
+                      Text(
+                        weather.name,
+                        style: TextStyles.h1,
+                      ),
+
+                      const SizedBox(height: 20),
+
+                      // Today's date
+                      Text(
+                        DateTime.now().dateTime,
+                        style: TextStyles.subtitleText,
+                      ),
+
+                      const SizedBox(height: 50),
+
+                      // Weather icon big
+                      SizedBox(
+                        height: 300,
+                        child: Image.asset(
+                          'assets/icons/${weather.weather[0].icon.replaceAll('n', 'd')}.png',
+                          fit: BoxFit.contain,
+                        ),
+                      ),
+
+                      const SizedBox(height: 50),
+
+                      // Weather description
+                      Text(
+                        weather.weather[0].description.capitalize,
+                        style: TextStyles.h2,
+                      ),
+                    ],
                   ),
-                  // Country name text
-                  Text(
-                    weather.name,
-                    style: TextStyles.h1,
-                  ),
 
-                  const SizedBox(height: 20),
+                  const SizedBox(height: 40),
 
-                  // Today's date
-                  Text(
-                    DateTime.now().dateTime,
-                    style: TextStyles.subtitleText,
-                  ),
+                  // Weather info in a row
+                  WeatherInfo(weather: weather),
 
-                  const SizedBox(height: 50),
-
-                  // Weather icon big
-                  SizedBox(
-                    height: 300,
-                    child: Image.asset(
-                      'assets/icons/${weather.weather[0].icon.replaceAll('n', 'd')}.png',
-                      fit: BoxFit.contain,
-                    ),
-                  ),
-
-                  const SizedBox(height: 50),
-
-                  // Weather description
-                  Text(
-                    weather.weather[0].description.capitalize,
-                    style: TextStyles.h2,
-                  ),
+                  const SizedBox(height: 15),
                 ],
-              ),
-
-              const SizedBox(height: 40),
-
-              // Weather info in a row
-              WeatherInfo(weather: weather),
-
-              const SizedBox(height: 15),
-            ],
-          );
-        },
-        error: (error, stackTrace) {
-          return const Center(
-            child: Text(
-              'An error has occurred',
+              );
+            },
+            error: (error, stackTrace) {
+              return const Center(
+                child: Text(
+                  'An error has occurred',
+                ),
+              );
+            },
+            loading: () {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            },
+          ),
+          Positioned(
+            top: 15,
+            left: 15,
+            child: IconButton(
+              icon: const Icon(Icons.arrow_back, color: Colors.white),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
             ),
-          );
-        },
-        loading: () {
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
-        },
+          ),
+        ],
       ),
     );
   }
